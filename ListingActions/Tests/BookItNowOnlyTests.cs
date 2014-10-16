0a655f30dc87aa06ee0b6261d3ddcf4ef4c97163
'jvinyard' <jvinyard@uship.com>
2014-10-16 16:41:39 -0500

    Add tests for a couple more historical listing formats

--- ListingActions/Tests/BookItNowOnlyTests.cs
+++ ListingActions/Tests/BookItNowOnlyTests.cs
@@ -1,35 +1,96 @@
-﻿using ListingActions.Contexts;
-using ListingActions.Entities;
+﻿using System.Linq;
+using ListingActions.Contexts;
 using ListingActions.Pipeline;
+using ListingActions.Specs;
 using NUnit.Framework;
 
 namespace ListingActions.Tests
 {
     [TestFixture]
-    public class BookItNowOnlyTests
+    public class BookItNowOnlyTests : BaseTests
     {
-
-        private IPipeline<BiddingContext> _pipeline;
-        private IDatabase _database;
-        private User _shipper;
-        private Listing _listing;
-
-        [SetUp]
-        public void SetUp()
+        protected override IPipeline<BiddingContext> BuildPipeline()
         {
-                
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            
+            return new Pipeline<BiddingContext>(
+                new PlaceBid(
+                    new BidderIsIdVerified(),
+                    new UserHasStarRating(5),
+                    new BidAmountIsEqualTo(new Price(999m)),
+                    new ListingHasCreditCard()
+                ),
+                new PutListingOnHold(
+                    new All()
+                ),
+                new AutoAcceptBid(
+                    new All()
+                )
+            );
         }
 
         [Test]
-        public void Write_me()
+        public void Can_fetch_pricing_requirements()
         {
-            Assert.Fail();
+            var req = _pipeline.SpecsAs<IPriceRestriction>().FirstOrDefault();
+            Assert.NotNull(req);
+            Assert.AreEqual(new Price(999m), req.Max);
+            Assert.AreEqual(new Price(999m), req.Min);
+        }
+
+        [Test]
+        public void Non_id_verified_bidder_sees_no_bid_permission()
+        {
+            var bidder = BuildBidder();
+            var context = BuildContext(bidder);
+            var steps = _pipeline.ReachableSteps(
+                context, SpecQuery<BiddingContext>.WithDimension<IPreBidContext>())
+                .ToList();
+            AssertNoSteps(steps);
+        }
+
+        [Test]
+        public void Id_verified_bidder_with_low_star_rating_cannot_place_bid()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 1);
+            var context = BuildContext(bidder);
+            var steps = _pipeline.ReachableSteps(
+                context, SpecQuery<BiddingContext>.WithDimension<IPreBidContext>())
+                .ToList();
+            AssertNoSteps(steps);
+        }
+
+        [Test]
+        public void Five_star_bidder_sees_auto_accept_permission()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 5.5);
+            var context = BuildContext(bidder);
+            var steps = _pipeline.ReachableSteps(
+                context, SpecQuery<BiddingContext>.WithDimension<IPreBidContext>())
+                .ToList();
+            AssertStepsEqual(
+                new[] { PLACE_BID, PUT_ON_HOLD, AUTO_ACCEPT }, steps);
+        }
+
+        [Test]
+        public void Five_star_bidder_who_bids_at_wrong_price_fails_to_place_bid()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 5.5);
+            var context = BuildContext(bidder, 997m);
+            var steps = _pipeline.Execute(context).ToList();
+            AssertNoSteps(steps);
+            AssertListingStatusIs(ListingStatus.Active);
+            AssertNoBidWasPlaced(bidder);
+        }
+
+        [Test]
+        public void Five_star_bidder_who_bids_at_correct_price_books_listing()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 5.5);
+            var context = BuildContext(bidder, 999m);
+            var steps = _pipeline.Execute(context).ToList();
+            AssertStepsEqual(
+                new[] { PLACE_BID, PUT_ON_HOLD, AUTO_ACCEPT }, steps);
+            AssertListingStatusIs(ListingStatus.Booked);
+            AssertBidStatusIs(BidStatus.Accepted, bidder);
         }
     }
 }
--- ListingActions/Tests/OfferPriceAndBookItNowTests.cs
+++ ListingActions/Tests/OfferPriceAndBookItNowTests.cs
@@ -1,5 +1,5 @@
-﻿using ListingActions.Contexts;
-using ListingActions.Entities;
+﻿using System.Linq;
+using ListingActions.Contexts;
 using ListingActions.Pipeline;
 using ListingActions.Specs;
 using NUnit.Framework;
@@ -7,41 +7,99 @@ using NUnit.Framework;
 namespace ListingActions.Tests
 {
     [TestFixture]
-    public class OfferPriceAndBookItNowTests
+    public class OfferPriceAndBookItNowTests : BaseTests
     {
-        private IPipeline<BiddingContext> _pipeline;
-        private IDatabase _database;
-        private User _shipper;
-        private Listing _listing;
-
-        [SetUp]
-        public void SetUp()
+        protected override IPipeline<BiddingContext> BuildPipeline()
         {
-            _pipeline = new Pipeline<BiddingContext>(
+            return new Pipeline<BiddingContext>(
                 new PlaceBid(
                     new BidderIsIdVerified(),
                     new BidAmountIsEqualTo(new Price(456m))
-                ),
+                    ),
                 new PutListingOnHold(
                     new All()
-                ),
+                    ),
                 new AutoAcceptBid(
                     new ListingHasCreditCard(),
                     new UserHasStarRating(5)
-                )
-            );    
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            
+                    )
+                );
         }
 
         [Test]
-        public void Write_me()
+        public void Can_fetch_pricing_requirements()
         {
-            Assert.Fail();
+            var req = _pipeline.SpecsAs<IPriceRestriction>().FirstOrDefault();
+            Assert.NotNull(req);
+            Assert.AreEqual(new Price(456m), req.Min);
+            Assert.AreEqual(new Price(456m), req.Max);
+        }
+
+        [Test]
+        public void Non_id_verified_user_sees_no_bid_step()
+        {
+            var bidder = BuildBidder();
+            var context = BuildContext(bidder);
+            var steps = _pipeline.ReachableSteps(
+                context, SpecQuery<BiddingContext>.WithDimension<IPreBidContext>())
+                .ToList();
+            AssertNoSteps(steps);
+        }
+
+        [Test]
+        public void Id_verified_user_sees_on_hold_step()
+        {
+            var bidder = BuildBidder(idVerified: true);
+            var context = BuildContext(bidder);
+            var steps = _pipeline.ReachableSteps(
+                context, SpecQuery<BiddingContext>.WithDimension<IPreBidContext>())
+                .ToList();
+            AssertStepsEqual(new[] { PLACE_BID, PUT_ON_HOLD }, steps);
+        }
+
+        [Test]
+        public void Five_star_user_sees_auto_accept_step()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 5.1);
+            var context = BuildContext(bidder);
+            var steps = _pipeline.ReachableSteps(
+                context, SpecQuery<BiddingContext>.WithDimension<IPreBidContext>())
+                .ToList();
+            AssertStepsEqual(
+                new[] { PLACE_BID, PUT_ON_HOLD, AUTO_ACCEPT }, steps);
+        }
+
+        [Test]
+        public void Bid_at_the_wrong_price_is_not_placed()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 5.1);
+            var context = BuildContext(bidder, 500m);
+            var steps = _pipeline.Execute(context).ToList();
+            AssertNoSteps(steps);
+            AssertListingStatusIs(ListingStatus.Active);
+            AssertNoBidWasPlaced(bidder);
+        }
+
+        [Test]
+        public void Bid_at_the_right_price_by_four_star_user_puts_listing_on_hold()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 4.1);
+            var context = BuildContext(bidder, 456m);
+            var steps = _pipeline.Execute(context).ToList();
+            AssertStepsEqual(new[] { PLACE_BID, PUT_ON_HOLD }, steps);
+            AssertListingStatusIs(ListingStatus.OnHold);
+            AssertBidStatusIs(BidStatus.Pending, bidder);
+        }
+
+        [Test]
+        public void Bid_at_the_right_price_by_five_star_user_books_listing()
+        {
+            var bidder = BuildBidder(idVerified: true, starRating: 5.1);
+            var context = BuildContext(bidder, 456m);
+            var steps = _pipeline.Execute(context).ToList();
+            AssertStepsEqual(new[] { PLACE_BID, PUT_ON_HOLD, AUTO_ACCEPT }, steps);
+            AssertListingStatusIs(ListingStatus.Booked);
+            AssertBidStatusIs(BidStatus.Accepted, bidder);
         }
     }
 }
